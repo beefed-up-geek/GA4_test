3f6999c7c8d40e24668ea73e663bd04f0c14ccb6
drdr3040 <110738697+drdr3040@users.noreply.github.com>
2024-08-06 10:55:59 +0900

    ojh202408061055

--- src/screen/medicine/Login/index.js
+++ src/screen/medicine/Login/index.js
@@ -1,49 +1,63 @@
-import {
-  GoogleSignin,
-  statusCodes,
-} from '@react-native-google-signin/google-signin';
-import React from 'react';
-import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
-import {View, Text} from 'react-native';
-
-import {GoogleAuthProvider, signInWithCredential} from 'firebase/auth';
-
-// Somewhere in your code
+import {useState} from 'react';
+import {View, Pressable, Image, Text} from 'react-native';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 GoogleSignin.configure({
   webClientId:
-    '1040523980047-5st7a3ianeqp28ilbg24e8veguku1hie.apps.googleusercontent.com', // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
+    '553674684367-g30th1q22jbqjs30jgad63i95vdntcmu.apps.googleusercontent.com',
+  androidClientId:
+    '553674684367-emj97ff7kjitq1qbn03ok9hebps9ijsg.apps.googleusercontent.com',
+  iosClientId:
+    '553674684367-sr2m1jems5sai07qgq710dvhdoqm6npv.apps.googleusercontent.com',
+  scopes: ['profile', 'email'],
 });
 
-const LoginScreen = ({}) => {
-  const signIn = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      const {idToken} = await GoogleSignin.signIn();
-      const googleCredentials = GoogleAuthProvider.credential(idToken);
-      await signInWithCredential(googleCredentials);
-    } catch (error) {
-      console.log('got error: ', error.message);
-      if (isErrorWithCode(error)) {
-        switch (error.code) {
-          case statusCodes.SIGN_IN_CANCELLED:
-            // user cancelled the login flow
-            break;
-          case statusCodes.IN_PROGRESS:
-            // operation (eg. sign in) already in progress
-            break;
-          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
-            // play services not available or outdated
-            break;
-          default:
-          // some other error happened
-        }
-      } else {
-        // an error that's not related to google sign in occurred
-      }
-    }
-  };
-  return <GoogleSigninButton onPress={() => signIn()} />;
+const GoogleLogin = async () => {
+  await GoogleSignin.hasPlayServices();
+  const userInfo = await GoogleSignin.signIn();
+  return userInfo;
 };
 
-export default LoginScreen;
+export default function LoginScreen() {
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState('');
+
+  const handleGoogleLogin = async () => {
+    setLoading(true);
+    try {
+      const response = await GoogleLogin();
+      const {idToken, user} = response;
+      console.log(user);
+      setUser(user);
+
+      //if (idToken) {
+      //  const resp = await authAPI.validateToken({
+      //    token: idToken,
+      //    email: user.email,
+      //  });
+      //  await handlePostLoginData(resp.data);
+      //}
+    } catch (apiError) {
+      setError(
+        apiError?.response?.data?.error?.message || 'Something went wrong',
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={{margin: 100}}>
+      <Pressable onPress={handleGoogleLogin}>
+        <Text>Continue with Google</Text>
+      </Pressable>
+      <Image
+        style={{height: 50, width: 50}}
+        resizeMode="cover"
+        source={{uri: user?.photo}}
+      />
+      <Text>{user?.name}</Text>
+    </View>
+  );
+}
