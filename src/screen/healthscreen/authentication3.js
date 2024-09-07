96b45cb92978622ddc46710869f31204a13ede4c
drdr3040 <110738697+drdr3040@users.noreply.github.com>
2024-09-08 07:19:02 +0900

    for pull

--- src/screen/healthscreen/authentication3.js
+++ src/screen/healthscreen/authentication3.js
@@ -1,7 +1,7 @@
 // /src/screen/health_screen/authentication3.js
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
-import { useRoute, useNavigation } from '@react-navigation/native';
+import {View, Text, TouchableOpacity, StyleSheet, Alert} from 'react-native';
+import {useRoute, useNavigation} from '@react-navigation/native';
 import FastImage from 'react-native-fast-image';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -9,7 +9,15 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const Authentication3Screen = () => {
   const route = useRoute();
   const navigation = useNavigation();
-  const { jti, twoWayTimestamp, name, birthdate, phoneNo, telecom, loginTypeLevel } = route.params;
+  const {
+    jti,
+    twoWayTimestamp,
+    name,
+    birthdate,
+    phoneNo,
+    telecom,
+    loginTypeLevel,
+  } = route.params;
 
   const handleCompleteAuth = async () => {
     try {
@@ -20,25 +28,31 @@ const Authentication3Screen = () => {
         telecom: telecom,
         loginTypeLevel: loginTypeLevel,
         jti: jti,
-        twoWayTimestamp: twoWayTimestamp
+        twoWayTimestamp: twoWayTimestamp,
       };
-      const response = await axios.post('https://27f0-203-252-33-4.ngrok-free.app/health_checkup/step2', request_data);
+      const response = await axios.post(
+        'https://8e74-203-252-33-3.ngrok-free.app/health_checkup/step2',
+        request_data,
+      );
       Alert.alert('성공', '인증이 완료되었습니다.');
-      
+
       // 오늘 날짜를 AsyncStorage에 저장
       const today = new Date().toISOString().split('T')[0]; // YYYY-MM-DD 형식으로 저장
       await AsyncStorage.setItem('healthscreen_last_update', today);
 
       // filteredData를 AsyncStorage에 저장
       const filteredData = response.data.filteredData;
-      await AsyncStorage.setItem('healthscreen_data', JSON.stringify(filteredData));
+      await AsyncStorage.setItem(
+        'healthscreen_data',
+        JSON.stringify(filteredData),
+      );
 
       // 저장된 값을 가져와서 출력
       const storedDate = await AsyncStorage.getItem('healthscreen_last_update');
       const storedData = await AsyncStorage.getItem('healthscreen_data');
-      console.log("<< storedDate >>");
+      console.log('<< storedDate >>');
       console.log(storedDate);
-      console.log("<< storedData >>");
+      console.log('<< storedData >>');
       console.log(JSON.parse(storedData));
 
       navigation.navigate('Health'); // 건강검진 홈화면으로 가기
@@ -50,12 +64,16 @@ const Authentication3Screen = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>이 화면은 /src/screen/health_screen/authentication3.js 🎉</Text>
-      <Text style={styles.waitingText}>본인인증을 완료하고 인증완료를 눌러주세요!</Text>
+      <Text style={styles.text}>
+        이 화면은 /src/screen/health_screen/authentication3.js 🎉
+      </Text>
+      <Text style={styles.waitingText}>
+        본인인증을 완료하고 인증완료를 눌러주세요!
+      </Text>
       <Text style={styles.waitingText}>기다리는 중입니다!</Text>
-      <FastImage 
-        source={require('../../images/health_screen/waiting.gif')} 
-        style={styles.image} 
+      <FastImage
+        source={require('../../images/health_screen/waiting.gif')}
+        style={styles.image}
         resizeMode={FastImage.resizeMode.contain}
       />
       <TouchableOpacity style={styles.button} onPress={handleCompleteAuth}>
